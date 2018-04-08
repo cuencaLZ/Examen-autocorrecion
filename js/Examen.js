09ac304aeabab0253d422b57ec54e0b81c9f382b
Lazarocp <lazaro.cuenca8@gmail.com>
2018-04-09 00:28:52 +0200

    cambios 3

--- js/Examen.js
+++ js/Examen.js
@@ -21,4 +21,25 @@ function parseXml (){
 
 function gestionarXml(xmldoc){
   console.log(xmldoc);
+  var Contenedor= document.getElementById("exam")
+  var enunciado = document.createElement("enunciado")
+  enunciado.innerHTML = xmldoc.getElementsByTagName("text")[0].innerHTML
+  Contenedor.appendChild(enunciado)
+  for (i=0;i<4;i++){
+    var span = document.createElement("SPAN")
+    var Contenedor= document.getElementById("exam")
+    var typopregunta=  document.createElement(xmldoc.getElementsByTagName("question") [0].childNodes[1].innerHTML)
+    typopregunta.setAttribute("type", xmldoc.getElementsByTagName("option")[i].getAttribute("type"))
+    span.innerHTML = xmldoc.getElementsByTagName("option")[i].innerHTML
+  Contenedor.appendChild(document.createElement("br"))
+  Contenedor.appendChild(typopregunta)
+  Contenedor.appendChild(span)
+
+  }
+  
+  console.log(xmldoc.getElementsByTagName("question") [0].childNodes[1])
+  console.log(xmldoc.getElementsByTagName("option")[0].getAttribute("type"))
+  console.log(document.createElement(xmldoc.getElementsByTagName("question") [0].childNodes[2].innerHTML))
 }
+
+
